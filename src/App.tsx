80a04a2c7b8c2a76f3ff61db9e62fff007aafd4d
Danny Joseph <89165878+DevByDJ@users.noreply.github.com>
2024-01-13 21:00:57 -0500

    Refactor employee selection logic in App component to fix bug 3

--- src/App.tsx
+++ src/App.tsx
@@ -63,9 +63,9 @@ export function App() {
           onChange={async (newValue) => {
             if (newValue === null) {
               return
-            }
-
-            await loadTransactionsByEmployee(newValue.id)
+            }if (newValue?.id != "") {
+              await loadTransactionsByEmployee(newValue?.id)
+            } else await loadAllTransactions()
           }}
         />
 
